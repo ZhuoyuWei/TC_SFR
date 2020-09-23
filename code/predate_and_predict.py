45c03790d78a34998728f448424ef2abbfe4a733
zhuwe <zhuwe@microsoft.com>
2020-09-23 22:45:43 +0800

    test submission 10 more data|back to 50|+ignore|test only first 10

--- code/predate_and_predict.py
+++ code/predate_and_predict.py
@@ -410,7 +410,10 @@ def main():
                         #    "%Y-%m-%d") + "T" + time + "," + site.strip() + "," + date.strftime(
                         #    "%Y-%m-%dT%H") + ",TC+wzyxp_123,{},CFS".format(value)
                         #value=values[vcount]
-                        value=values[0]
+                        if vcount<10:
+                            value=values[vcount]
+                        else:
+                            value=values[9]
                         if site.strip() in ignore_sites:
                             value=0
                         fout.write((date + timedelta(days=day)).strftime(
