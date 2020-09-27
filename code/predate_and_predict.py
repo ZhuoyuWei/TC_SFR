546e634f16c16ba96434501294815d5d051c4236
zhuwe <zhuwe@microsoft.com>
2020-09-27 13:55:26 +0800

    test nn method|test 1

--- code/predate_and_predict.py
+++ code/predate_and_predict.py
@@ -432,9 +432,316 @@ def online_infer_simple(date, local2lgb_loaded_models, fillval, delta_days,
 
     return loc2res
 
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+from torch.optim import Adam
+import torch
+import numpy as np
+import pandas as pd
+import numpy as np
+import os
+import sys
+
+def read_ground_truth(filename):
+    df=pd.read_csv(filename)
+    print(df.columns)
+    #print('######')
+    locations=set(df["LocationID"])
+    #print(locations)
+
+    df["DateTime"]=pd.to_datetime(df["DateTime"])
+    #print(df["DateTime"])
+    df["Value"].astype('float')
+
+    loc2values={}
+
+    for loc in locations:
+        sub_df=df[df["LocationID"]==loc]
+        sub_df=sub_df.sort_values(by='DateTime',ignore_index=True)
+        #print('$$$$$$$$$$$')
+        #print(sub_df.head())
+
+        loc2values[loc]=sub_df
+
+    return df, loc2values
+
+def read_ground_truth_withoutfloat(filename):
+    df=pd.read_csv(filename)
+    print(df.columns)
+    #print('######')
+    locations=set(df["LocationID"])
+    #print(locations)
+
+    df["DateTime"]=pd.to_datetime(df["DateTime"])
+    #print(df["DateTime"])
+    #df["Value"].astype('float')
+
+    loc2values={}
+
+    for loc in locations:
+        sub_df=df[df["LocationID"]==loc]
+        sub_df=sub_df.sort_values(by='DateTime',ignore_index=True)
+        #print('$$$$$$$$$$$')
+        #print(sub_df.head())
+
+        loc2values[loc]=sub_df
+
+    return df, loc2values
+
+
+
+def convert_local2values(df):
+    locations=set(df["LocationID"])
+    loc2values={}
+    for loc in locations:
+        sub_df=df[df["LocationID"]==loc]
+        sub_df=sub_df.sort_values(by='DateTime',ignore_index=True)
+        loc2values[loc]=sub_df
+    return loc2values
+
+
+from tqdm import tqdm, trange
+
+
+class RNNForTimeSeries(nn.Module):
+    def __init__(self, hidden_size, layer_num):
+        super(RNNForTimeSeries, self).__init__()
+        self.hidden_size = hidden_size
+        self.rnn = nn.LSTM(input_size=1,
+                           hidden_size=hidden_size,
+                           num_layers=layer_num,
+                           batch_first=True)
+        self.dense = nn.Linear(hidden_size * layer_num, 1)
+        self.loss_fct = nn.MSELoss()
+
+    def _step(self, input, batch_size):
+
+        _, (hn, cn) = self.rnn(input)
+        hn = hn.reshape((batch_size, -1))
+
+        logits = self.dense(hn)
+
+        return logits
+
+    '''
+    def forward(self,input,target=None):
+        input_size=input.size()
+        batch_size=input_size[0]
+
+        _,(hn,cn)=self.rnn(input)
+        hn=hn.reshape((batch_size,-1))
+
+        logits=self.dense(hn)
+
+        if target is not None:
+            loss=self.loss_fct(logits,target)
+            return logits,loss
+        else:
+            return logits
+    '''
+
+    def forward(self, input, target=None, max_length=0):
+        input_size = input.size()
+        batch_size = input_size[0]
+
+        input = input.unsqueeze(-1).float()
+
+        if target is not None:
+            target_size = target.size()
+            max_length = target_size[-1]
+            target = target.unsqueeze(-1).float()
+
+        assert max_length > 0
+
+        outputs = []
+
+        keep_input = input
+        total_loss = 0.
+        for i in range(max_length):
+            # print('debug {}'.format(input.shape))
+            logits = self._step(input, batch_size)
+            outputs.append(logits)
+            if target is not None:
+                loss = self.loss_fct(logits, target[:, i])
+                total_loss += loss
+                input = torch.cat([input, target[:, i:i + 1]], dim=1)
+            else:
+                input = torch.cat([input, logits.unsqueeze(1)], dim=1)
+
+        if target is not None:
+            return outputs, total_loss
+        else:
+            return outputs
+
+    '''
+    def decodering(self,input,steps):
+        outputs=[]
+        keep_input=input
+        for i in range(steps):
+            logits=self.forward(input)
+            outputs.append(logits)
+            input = torch.cat([input,logits.unsequeeze(1)],dim=1)
+
+        return outputs
+    '''
+
+
+class TSDataset(torch.utils.data.Dataset):
+    'Characterizes a dataset for PyTorch'
+
+    def __init__(self, X, Y=None):
+        'Initialization'
+        self.X = X
+        self.Y = Y
+
+    def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.X)
+
+    def __getitem__(self, index):
+        'Generates one sample of data'
+        # Select sample
+
+        # x=self.X.iloc[index]
+        # y=self.Y.iloc[index]
+
+        x = self.X[index]
+        if self.Y is not None:
+            y = self.Y[index]
+            return x, y
+
+        else:
+            return x
+
+
+def ftrain(train_input, train_target,
+           Epoch, batch_size, lr,
+           beta1, beta2, weight_decay,
+           save_model_path='model.json'):
+    trainset = TSDataset(X=train_input, Y=train_target)
+    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, sampler=None,
+                             batch_sampler=None, num_workers=0, collate_fn=None,
+                             pin_memory=False, drop_last=False, timeout=0,
+                             worker_init_fn=None)
+
+    model = RNNForTimeSeries(50, 3)
+    if torch.cuda.is_available():
+        model.cuda()
+
+    optimizer = Adam(model.parameters(), lr, betas=(beta1, beta2),
+                     weight_decay=weight_decay)
+
+    train_iterator = trange(Epoch, desc="Epoch")
+    for _ in train_iterator:
+        epoch_iterator = tqdm(trainloader, desc="Iteration")
+        for step, batch in enumerate(epoch_iterator):
+
+            (x, y) = batch
+            if torch.cuda.is_available():
+                x = x.cuda()
+                y = y.cuda()
+
+            # print(x.device)
+            # print(x)
+
+            optimizer.zero_grad()
+            # optimizer.cuda()
+
+            logits, loss = model(input=x, target=y)
+
+            if torch.is_tensor(loss):
+                loss.backward()
+                optimizer.step()
+
+    torch.save(model.state_dict(), save_model_path)
+
+def fpredict(test_input,
+             batch_size,
+           model,
+           max_decode_length=40):
+    testset = TSDataset(X=test_input)
+    testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, sampler=None,
+                             batch_sampler=None, num_workers=0, collate_fn=None,
+                             pin_memory=False, drop_last=False, timeout=0,
+                             worker_init_fn=None)
+
+
+    if torch.cuda.is_available():
+        model.cuda()
+
+
+
+    epoch_iterator = tqdm(testloader, desc="Iteration")
+
+    res=None
+
+    for step, batch in enumerate(epoch_iterator):
+
+            x = batch
+            if torch.cuda.is_available():
+                x = x.cuda()
+
+
+
+            logits= model(input=x,max_length=max_decode_length)
+            logits=np.concatenate([x.cpu().detach().numpy() for x in logits],1)
+            print(logits.shape)
+
+            if res is None:
+                res=logits
+            else:
+                res=np.concatenate((res,logits))
+
+    return res
+
+def load_model(filename,seqlen=50,layers=3):
+    model = RNNForTimeSeries(seqlen, layers)
+    model.load_state_dict(torch.load(filename))
+    if torch.cuda.is_available():
+        model.cuda()
+    return model
+
+def loading_models_rnn(model_dir,locations):
+    local2models={}
+    for local in locations:
+        model=load_model(os.path.join(model_dir,'{}_model.json'.format(local)))
+        local2models[local]=model
+    return local2models
+
+
+
+
+# generate sequence data for local by slide window
+def produce_data(df, train_len, test_len=40):
+    total_len = train_len + test_len
+    loc2df = convert_local2values(df)
+    loc2data = {}
+    for loc in loc2df:
+        sub_df = loc2df[loc]
+        values = sub_df['Value'].to_numpy()
+
+        data_list = []
+        for i in range(values.size - total_len):
+            data = values[i:i + total_len]
+            data_list.append(data)
+
+        loc2data[loc] = np.array(data_list)
+
+    return loc2data
+
+locations=['BNDN5','ARWN8','TCCC1','CARO2','ESSC2','NFDC1','LABW4','CLNK1','TRAC2','NFSW4']
+
+
+
+
+
+
+
+
+
 
 def online_infer_rnn(date, local2lgb_loaded_model, fillval, delta_days,
-                        max_feature, output_dir,feature_size,max_size=5):
+                        max_feature, output_dir,feature_size):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     filename = download_pre_several_days(date, delta_days, output_dir)
@@ -444,18 +751,23 @@ def online_infer_rnn(date, local2lgb_loaded_model, fillval, delta_days,
     #num_cols = ["f_{}".format(i) for i in range(1, max_feature + 1)]
     #preds = []
 
+    df_x=df_x[:,:feature_size]
+
     loc2res={}
     for i in range(len(locations)):
         loc=locations[i]
         loc2res[loc]=[]
-        models=local2lgb_loaded_models[loc]
-        for j in range(feature_size):
-            if j < max_size:
-                lgb_loaded_model = models[j]
-                dev_y_pred_local = lgb_loaded_model.predict(df_x.iloc[i])
-                loc2res[loc].append(dev_y_pred_local[0])
-            else:
-                loc2res[loc].append(loc2res[loc][-1])
+        model=local2lgb_loaded_model[loc]
+
+
+
+        res=fpredict(df_x,
+                 1,
+                 model,
+                 max_decode_length=40)
+
+
+        loc2res[loc]=res
 
     return loc2res
 
@@ -480,7 +792,9 @@ def main():
     for site in target_sites:
         locations.append(site.strip())
     #local2lgb_loaded_model= loading_models_locally(r'/work/',locations)
-    local2lgb_loaded_model = loading_models_400(r'/work/', locations)
+    #local2lgb_loaded_model = loading_models_400(r'/work/', locations)
+    local2models = loading_models_rnn(r'/work/', locations)
+
 
 
     fillval = 0
@@ -506,7 +820,7 @@ def main():
         for date in target_dates:
 
             #local2latest = download_pre_day(date,output_dir)
-            local2res = online_infer_simple(date, local2lgb_loaded_model, fillval, delta_days,
+            local2res = online_infer_simple(date, local2models, fillval, delta_days,
                                             max_feature, output_dir,60,1)
 
             for site in target_sites:
