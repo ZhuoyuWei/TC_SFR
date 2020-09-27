6c786ba4a062cf73f098cf0db0f5e2eef793ccc6
zhuwe <zhuwe@microsoft.com>
2020-09-27 14:48:10 +0800

    test nn method|test 2

--- code/test.sh
+++ code/test.sh
@@ -7,6 +7,6 @@ pip3 install lightgbm
 pip3 install category-encoders
 pip3 install pickle-mixin
 pip3 install tqdm
-pip3 install torch torchvision --upgrade
+pip3 install torch==1.6.0+cpu torchvision==0.7.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 
 python3 predate_and_predict.py $1 $2
