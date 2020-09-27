675f49d04d35a565bcb562b8ea06f6e92d4b035d
zhuwe <zhuwe@microsoft.com>
2020-09-27 18:58:49 +0800

    test nn method|test 10

--- code/predate_and_predict.py
+++ code/predate_and_predict.py
@@ -766,10 +766,10 @@ def online_infer_rnn(date, local2lgb_loaded_model, fillval, delta_days,
                                                fillval)
     #num_cols = ["f_{}".format(i) for i in range(1, max_feature + 1)]
     #preds = []
-
+    loc2data=convert_local2values(df_x)
     #df_x=df_x[:,:feature_size]
 
-    df_x=df_x.to_numpy()
+    #df_x=df_x.to_numpy()
     print('input df_x shape={}',format(df_x.shape))
 
     loc2res={}
@@ -777,10 +777,12 @@ def online_infer_rnn(date, local2lgb_loaded_model, fillval, delta_days,
         loc=locations[i]
         loc2res[loc]=[]
         model=local2lgb_loaded_model[loc]
+        df=loc2data[loc].to_numpy()
+        print('input local df_x shape={}', format(df.shape))
 
 
 
-        res=fpredict(df_x,
+        res=fpredict(df,
                  1,
                  model,
                  max_decode_length=40)
